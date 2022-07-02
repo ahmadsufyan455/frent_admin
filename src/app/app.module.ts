eeced0a0a2758fd8701aeaf950d852fc17ca021b
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2022-07-02 16:32:43 +0700

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddMotorComponent } from './components/add-motor/add-motor.component';
@@ -16,7 +21,10 @@ import { MotorListComponent } from './components/motor-list/motor-list.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/motor.model.ts
+++ src/app/models/motor.model.ts
@@ -1,2 +1,9 @@
 export class Motor {
+    id?: String;
+    image?: String;
+    type?: String;
+    description?: String;
+    price?: number;
+    quantity?: number;
+    status?: boolean;
 }
